00ccace8e85e327f85cbccdf4165b8361905d84b
mx417 <mx476293998@163.com>
2019-12-10 16:40:08 +0800

    数据加密

--- src/main/java/com/zendesk/maxwell/replication/BinlogConnectorEvent.java
+++ src/main/java/com/zendesk/maxwell/replication/BinlogConnectorEvent.java
@@ -6,6 +6,10 @@ import com.zendesk.maxwell.row.RowMap;
 import com.zendesk.maxwell.schema.Table;
 import com.zendesk.maxwell.schema.columndef.ColumnDef;
 
+import static com.zendesk.maxwell.util.EncodeCacheTaskManager.encodeColsMap;
+import com.zendesk.maxwell.util.Md5Encode;
+import com.zendesk.maxwell.util.PhoneEncode;
+
 import java.io.Serializable;
 import java.util.*;
 
@@ -108,21 +112,91 @@ public class BinlogConnectorEvent {
 	private void writeData(Table table, RowMap row, Serializable[] data, BitSet includedColumns) {
 		int dataIdx = 0, colIdx = 0;
 
-		for ( ColumnDef cd : table.getColumnList() ) {
-			if ( includedColumns.get(colIdx) ) {
-				Object json = null;
-				if ( data[dataIdx] != null ) {
-					json = cd.asJSON(data[dataIdx], outputConfig);
+		//加密
+		String key = (table.getDatabase() + "." + table.getName()).toLowerCase();
+		if (encodeColsMap.containsKey(key)){
+			Map<String, Integer> mp = encodeColsMap.get(key);
+			for (ColumnDef cd : table.getColumnList() ) {
+				if ( includedColumns.get(colIdx) ) {
+					Object json = null;
+					if ( data[dataIdx] != null ) {
+						json = cd.asJSON(data[dataIdx], outputConfig);
+						if ((cd.getName() != null) && mp.containsKey(cd.getName().toLowerCase())){
+							String d = json.toString();
+							switch (mp.get(cd.getName().toLowerCase())){
+								case 1:
+									json = PhoneEncode.encryption(d, "SHA-256", d);
+									break;
+								case 2:
+									json = Md5Encode.encryption(d, "SHA", d);
+									break;
+							}
+						}
+					}
+					row.putData(cd.getName(), json);
+					dataIdx++;
 				}
-				row.putData(cd.getName(), json);
-				dataIdx++;
+				colIdx++;
 			}
-			colIdx++;
+		} else {
+			for ( ColumnDef cd : table.getColumnList() ) {
+				if ( includedColumns.get(colIdx) ) {
+					Object json = null;
+					if ( data[dataIdx] != null ) {
+						json = cd.asJSON(data[dataIdx], outputConfig);
+					}
+					row.putData(cd.getName(), json);
+					dataIdx++;
+				}
+				colIdx++;
+			}
 		}
 	}
 
 	private void writeOldData(Table table, RowMap row, Serializable[] oldData, BitSet oldIncludedColumns) {
 		int dataIdx = 0, colIdx = 0;
+
+		//加密
+		String key = (table.getDatabase() + "." + table.getName()).toLowerCase();
+		if (encodeColsMap.containsKey(key)){
+			Map<String, Integer> mp = encodeColsMap.get(key);
+			for ( ColumnDef cd : table.getColumnList() ) {
+				if ( oldIncludedColumns.get(colIdx) ) {
+					Object json = null;
+					if ( oldData[dataIdx] != null ) {
+						json = cd.asJSON(oldData[dataIdx], outputConfig);
+						if ((cd.getName() != null) && mp.containsKey(cd.getName().toLowerCase())){
+							String d = json.toString();
+							switch (mp.get(cd.getName().toLowerCase())){
+								case 1:
+									json = PhoneEncode.encryption(d, "SHA-256", d);
+									break;
+								case 2:
+									json = Md5Encode.encryption(d, "SHA", d);
+									break;
+							}
+						}
+					}
+
+					if (!row.hasData(cd.getName())) {
+					/*
+					   If we find a column in the BEFORE image that's *not* present in the AFTER image,
+					   we're running in binlog_row_image = MINIMAL.  In this case, the BEFORE image acts
+					   as a sort of WHERE clause to update rows with the new values (present in the AFTER image),
+					   In this case we should put what's in the "before" image into the "data" section, not the "old".
+					 */
+						row.putData(cd.getName(), json);
+					} else {
+						if (!Objects.equals(row.getData(cd.getName()), json)) {
+							row.putOldData(cd.getName(), json);
+						}
+					}
+					dataIdx++;
+				}
+				colIdx++;
+			}
+		}
+
 
 		for ( ColumnDef cd : table.getColumnList() ) {
 			if ( oldIncludedColumns.get(colIdx) ) {
